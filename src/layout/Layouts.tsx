cfe6ad31ab78fc8b6e1c22979cf6f11413f4c106
Harsh412589 <hkavaiya001@gmail.com>
2024-09-18 09:50:28 +0530

    new updates

--- src/layout/Layouts.tsx
+++ src/layout/Layouts.tsx
@@ -2,24 +2,25 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-const Mainlayout = () => {
+const MainLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen m-0">
+    <div className="flex flex-col min-h-screen w-full overflow-x-hidden">
       {/* Navbar */}
-      <header>
+      <header className="w-full">
         <Navbar />
       </header>
-      {/* main screen */}
-      <div className="flex-1">
-        <Outlet />
-      </div>
 
-      {/* footer */}
-      <footer>
+      {/* Main content area */}
+      <main className="flex-1 w-full">
+        <Outlet />
+      </main>
+
+      {/* Footer */}
+      <footer className="w-full">
         <Footer />
       </footer>
     </div>
   );
 };
 
-export default Mainlayout;
+export default MainLayout;
